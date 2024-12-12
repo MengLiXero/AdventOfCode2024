dc95cb1a92ee2adf070d06b268d00f01469593d3
Meng Li <meng.li@xero.com>
2024-12-13 08:03:26 +1300

    Day12 Part2

--- Project/Day12/Day12.cs
+++ Project/Day12/Day12.cs
@@ -7,10 +7,11 @@ public class Day12 : IAocDay
 {
     private static string[] _input;
     private static char[][] _grid;
-    private static Dictionary<(char,Vector2), (int, int)> garden = new() { };
+    private static Dictionary<(char, Vector2), (int, int)> garden = new() { };
+    private static Dictionary<(char, Vector2), HashSet<(Vector2,string)>> regionEdges = new() { };
     private static int _price;
     private static bool _isPartTwo;
-    
+
     private static void Initialize()
     {
         _input = File.ReadAllLines(Constants.baseDir + "Day12/data-aoc-day12.txt");
@@ -19,7 +20,7 @@ public class Day12 : IAocDay
             .ToArray();
         _grid = AddBorder();
     }
-    
+
     private static char[][] AddBorder(char boarderElement = '.')
     {
         // Create the top and bottom border row
@@ -42,103 +43,127 @@ public class Day12 : IAocDay
         {
             Console.WriteLine(string.Join(" ", c));
         }
-        
-        for (int row = 1; row < _grid.Length-1; row++)
-        {
-            for(int col = 1; col < _grid[row].Length-1; col++)
-            {
-                var plotType = _grid[row][col];
-                // if(plotType == '.')
-                //     continue;
-                //Check if the current plot is exist in the garden(use LINQ), if so
-                    //Check if current plot can lead to the plant lead. If so
-                        //Check how many edges are adjacent to a plot of different plant.
-                        //Add area(1) and edges to current record in the garden
-                    //else
-                        //Check how many edges are adjacent to a plot of different plant.
-                        //Add current plot to the garden as a plant lead
-                //else
-                    //Check how many edges are adjacent to a plot of different plant.
-                    //Add current plot to the garden as a plant lead
 
-                    var matchingPlot = garden.Where(record => record.Key.Item1 == plotType);
-                    var matched = false;
-                    if (matchingPlot.Count() != 0)
-                    {
-                        foreach (var match in matchingPlot)
-                        {
-                            var originalAreaAndEdges = garden[(plotType, match.Key.Item2)];
-                            HashSet<Vector2> visited = new();
-                            visited.Add(new Vector2(row, col));
-                            var found = false;
-                            if (canTraverseToPlantLead(match.Key.Item2, new Vector2(row, col),plotType, visited, ref found))
-                            {
-                                var edges = checkEdges(plotType, new Vector2(row, col));
-                                garden[(plotType, match.Key.Item2)] = (originalAreaAndEdges.Item1 + 1,
-                                    originalAreaAndEdges.Item2 + edges);
-                                matched = true;
-                            }
-                        }
-
-                        if (!matched)
-                        {
-                            var edges = checkEdges(plotType, new Vector2(row,col));
-                            garden.Add((plotType, new Vector2(row, col)), (1, edges));
-                        }
-                    }
-                    else
-                    {
-                        var edges = checkEdges(plotType, new Vector2(row,col));
-                        garden.Add((plotType, new Vector2(row, col)), (1, edges));
-                    }
-            }
-        }
+        PopulateGarden();
 
         foreach (var region in garden)
         {
             _price += region.Value.Item1 * region.Value.Item2;
         }
-        
+
         Console.WriteLine($"Total price is {_price}");
-        
     }
 
-    private static bool canTraverseToPlantLead(Vector2 plantLead, Vector2 currentPlot, char plotType, HashSet<Vector2> visited, ref bool found)
+    private static void PopulateGarden()
+    {
+        for (int row = 1; row < _grid.Length - 1; row++)
+        {
+            for (int col = 1; col < _grid[row].Length - 1; col++)
+            {
+                var plotType = _grid[row][col];
+                if (plotType == 'O')
+                {
+                    Console.WriteLine("here");
+                }
+                //Check if the current plot is exist in the garden(use LINQ), if so
+                //Check if current plot can lead to the plant lead. If so
+                //Check how many edges are adjacent to a plot of different plant.
+                //Add area(1) and edges to current record in the garden
+                //else
+                //Check how many edges are adjacent to a plot of different plant.
+                //Add current plot to the garden as a plant lead
+                //else
+                //Check how many edges are adjacent to a plot of different plant.
+                //Add current plot to the garden as a plant lead
+
+                var matchingPlot = garden.Where(record => record.Key.Item1 == plotType);
+                var matched = false;
+                if (matchingPlot.Count() != 0)
+                {
+                    foreach (var match in matchingPlot)
+                    {
+                        var originalAreaAndEdges = garden[(plotType, match.Key.Item2)];
+                        HashSet<Vector2> visited = new();
+                        visited.Add(new Vector2(row, col));
+                        var found = false;
+                        if (canTraverseToPlantLead(match.Key.Item2, new Vector2(row, col), plotType, visited,
+                                ref found))
+                        {
+                            var edges = checkEdges((plotType, match.Key.Item2), new Vector2(row, col));
+                            garden[(plotType, match.Key.Item2)] = (originalAreaAndEdges.Item1 + 1,
+                                originalAreaAndEdges.Item2 + edges);
+                            matched = true;
+                        }
+                    }
+
+                    if (!matched)
+                    {
+                        var edges = checkEdges((plotType, new Vector2(row, col)), new Vector2(row, col));
+                        garden.Add((plotType, new Vector2(row, col)), (1, edges));
+                    }
+                }
+                else
+                {
+                    var edges = checkEdges((plotType, new Vector2(row, col)), new Vector2(row, col));
+                    garden.Add((plotType, new Vector2(row, col)), (1, edges));
+                }
+            }
+        }
+    }
+
+    private static bool canTraverseToPlantLead(Vector2 plantLead, Vector2 currentPlot, char plotType,
+        HashSet<Vector2> visited, ref bool found)
     {
         if (plantLead == currentPlot || found)
         {
             found = true;
-            return true; 
+            return true;
         }
 
-        if (_grid[(int)currentPlot.X-1][(int)currentPlot.Y] == plotType && !visited.Contains(new Vector2(currentPlot.X - 1, currentPlot.Y)))
+        if (_grid[(int)currentPlot.X - 1][(int)currentPlot.Y] == plotType &&
+            !visited.Contains(new Vector2(currentPlot.X - 1, currentPlot.Y)))
         {
             visited.Add(new Vector2(currentPlot.X - 1, currentPlot.Y));
             // Console.WriteLine("up");
-            if( canTraverseToPlantLead(plantLead, new Vector2(currentPlot.X - 1, currentPlot.Y), plotType, visited, ref found))
+            if (canTraverseToPlantLead(plantLead, new Vector2(currentPlot.X - 1, currentPlot.Y), plotType, visited,
+                    ref found))
             {
                 return true;
-            };
+            }
+
+            ;
         }
-        if (_grid[(int)currentPlot.X + 1][(int)currentPlot.Y] == plotType && !visited.Contains(new Vector2(currentPlot.X + 1, currentPlot.Y)))
+
+        if (_grid[(int)currentPlot.X + 1][(int)currentPlot.Y] == plotType &&
+            !visited.Contains(new Vector2(currentPlot.X + 1, currentPlot.Y)))
         {
             visited.Add(new Vector2(currentPlot.X + 1, currentPlot.Y));
             // Console.WriteLine("down");
-            if(canTraverseToPlantLead(plantLead, new Vector2(currentPlot.X + 1, currentPlot.Y), plotType, visited, ref found))
+            if (canTraverseToPlantLead(plantLead, new Vector2(currentPlot.X + 1, currentPlot.Y), plotType, visited,
+                    ref found))
             {
                 return true;
-            };
+            }
+
+            ;
         }
-        if (_grid[(int)currentPlot.X][(int)currentPlot.Y - 1] == plotType && !visited.Contains(new Vector2(currentPlot.X, currentPlot.Y - 1)))
+
+        if (_grid[(int)currentPlot.X][(int)currentPlot.Y - 1] == plotType &&
+            !visited.Contains(new Vector2(currentPlot.X, currentPlot.Y - 1)))
         {
             visited.Add(new Vector2(currentPlot.X, currentPlot.Y - 1));
             // Console.WriteLine("left");
-            if(canTraverseToPlantLead(plantLead, new Vector2(currentPlot.X, currentPlot.Y - 1), plotType, visited, ref found))
+            if (canTraverseToPlantLead(plantLead, new Vector2(currentPlot.X, currentPlot.Y - 1), plotType, visited,
+                    ref found))
             {
                 return true;
-            };
+            }
+
+            ;
         }
-        if (_grid[(int)currentPlot.X][(int)currentPlot.Y + 1] == plotType && !visited.Contains(new Vector2(currentPlot.X, currentPlot.Y + 1)))
+
+        if (_grid[(int)currentPlot.X][(int)currentPlot.Y + 1] == plotType &&
+            !visited.Contains(new Vector2(currentPlot.X, currentPlot.Y + 1)))
         {
             visited.Add(new Vector2(currentPlot.X, currentPlot.Y + 1));
             // Console.WriteLine("right");
@@ -146,24 +171,104 @@ public class Day12 : IAocDay
                     ref found))
             {
                 return true;
-            };
-        } 
-        return false; 
+            }
+
+            ;
+        }
+
+        return false;
     }
 
-    private static int checkEdges(char plotType, Vector2 currentPlot)
+    private static int checkEdges((char, Vector2) plotLead, Vector2 currentPlot)
     {
-        var topMatch = _grid[(int)currentPlot.X - 1][(int)currentPlot.Y] != plotType;
-        var bottomMatch = _grid[(int)currentPlot.X + 1][(int)currentPlot.Y] != plotType;
-        var leftMatch = _grid[(int)currentPlot.X][(int)currentPlot.Y - 1] != plotType;
-        var rightMatch = _grid[(int)currentPlot.X][(int)currentPlot.Y + 1] != plotType;
-        return (topMatch? 1 : 0) + (bottomMatch? 1 : 0) + (leftMatch? 1 : 0) + (rightMatch? 1 : 0);
+        var isTopEdge = _grid[(int)currentPlot.X - 1][(int)currentPlot.Y] != plotLead.Item1;
+        
+        var isBottomEdge = _grid[(int)currentPlot.X + 1][(int)currentPlot.Y] != plotLead.Item1;
+        
+        var isLeftEdge = _grid[(int)currentPlot.X][(int)currentPlot.Y - 1] != plotLead.Item1;
+        
+        var isRightEdge = _grid[(int)currentPlot.X][(int)currentPlot.Y + 1] != plotLead.Item1;
 
+        if (_isPartTwo)
+        {
+            var isNewTopEdge = isTopEdge && isEdgeCounted(plotLead, currentPlot, (-1, -1), (-1, 1), isTopEdge,"bottom");
+            if (isTopEdge)
+            {
+                if (regionEdges.ContainsKey(plotLead))
+                {
+                    regionEdges[plotLead].Add((new Vector2(currentPlot.X - 1, currentPlot.Y),"bottom"));
+                }
+                else
+                {
+                    regionEdges.Add(plotLead, new HashSet<(Vector2,string)> { (new Vector2(currentPlot.X - 1, currentPlot.Y),"bottom") });
+                }
+            }
+            var isNewBottomEdge = isBottomEdge && isEdgeCounted(plotLead, currentPlot, (1, -1), (1, 1), isBottomEdge,"top");
+            if (isBottomEdge)
+            {
+                if (regionEdges.ContainsKey(plotLead))
+                {
+                    regionEdges[plotLead].Add((new Vector2(currentPlot.X + 1, currentPlot.Y),"top"));
+                }
+                else
+                {
+                    regionEdges.Add(plotLead, new HashSet<(Vector2,string)> { (new Vector2(currentPlot.X + 1, currentPlot.Y),"top") });
+                }
+            }
+            var isNewLeftEdge = isLeftEdge && isEdgeCounted(plotLead, currentPlot, (-1, -1), (1, -1), isLeftEdge,"right");
+            if (isLeftEdge)
+            {
+                if (regionEdges.ContainsKey(plotLead))
+                {
+                    regionEdges[plotLead].Add((new Vector2(currentPlot.X, currentPlot.Y - 1),"right"));
+                }
+                else
+                {
+                    regionEdges.Add(plotLead, new HashSet<(Vector2,string)> { (new Vector2(currentPlot.X, currentPlot.Y - 1),"right") });
+                }
+            }
+            var isNewRightEdge = isRightEdge && isEdgeCounted(plotLead, currentPlot, (-1, 1), (1, 1), isRightEdge,"left");
+            if (isRightEdge)
+            {
+                if (regionEdges.ContainsKey(plotLead))
+                {
+                    regionEdges[plotLead].Add((new Vector2(currentPlot.X, currentPlot.Y + 1),"left"));
+                }
+                else
+                {
+                    regionEdges.Add(plotLead, new HashSet<(Vector2,string)> { (new Vector2(currentPlot.X, currentPlot.Y + 1),"left") });
+                }
+            }
+            return (isNewTopEdge ? 1 : 0) + (isNewBottomEdge ? 1 : 0) + (isNewLeftEdge ? 1 : 0) + (isNewRightEdge ? 1 : 0);
+        }
+
+        return (isTopEdge ? 1 : 0) + (isBottomEdge ? 1 : 0) + (isLeftEdge ? 1 : 0) + (isRightEdge ? 1 : 0);
+    }
+
+    private static bool isEdgeCounted((char, Vector2) plotLead, Vector2 currentPlot, (int, int) neighbour1,
+        (int, int) neighbour2, bool isEdge, string side)
+    {
+        if (!isEdge)
+        {
+            return false;
+        }
+
+        if (regionEdges.ContainsKey(plotLead)
+            && (regionEdges[plotLead]
+                    .Contains((new Vector2(currentPlot.X + neighbour1.Item1, currentPlot.Y + neighbour1.Item2),side))
+                || regionEdges[plotLead]
+                    .Contains((new Vector2(currentPlot.X + neighbour2.Item1, currentPlot.Y + neighbour2.Item2),side))))
+        {
+            isEdge = false;
+        }
+        
+        return isEdge;
     }
 
     public static void RunPart2()
     {
-        throw new NotImplementedException();
+        _isPartTwo = true;
+        RunPart1();
     }
 
     public void BenchmarkPart1()
